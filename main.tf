7b409e3e4f4acdad421949b3adcfd783864579d8
Yaru Anisovec <anisovets@gmail.com>
2023-10-01 12:36:56 -0400

    hardcoded region in backend configuration

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket = "yaru-labs-terraform-state"
     key = "iam-pipeline/terraform.tfstate"
-    region = var.region
+    region = "us-east-1"
     dynamodb_table = "TerraformLocks"
     encrypt = true
     
